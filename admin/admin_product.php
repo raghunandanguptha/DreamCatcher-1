80031c9cca7fca37bbb66a8d580ce4090ade0063
Guptha-01 <rguptha2000@gmail.com>
2021-06-06 17:48:56 +0530

    Initial commit

---
+++ admin/admin_product.php
@@ -0,0 +1,77 @@
+<?php
+	include("../function/session.php");
+	include("../db/dbconn.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dream Catchers Zone</title>
+	<link rel = "stylesheet" type = "text/css" href="../css/style.css" media="all">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<script src="../js/bootstrap.js"></script>
+	<script src="../js/jquery-1.7.2.min.js"></script>
+	<script src="../js/carousel.js"></script>
+	<script src="../js/button.js"></script>
+	<script src="../js/dropdown.js"></script>
+	<script src="../js/tab.js"></script>
+	<script src="../js/tooltip.js"></script>
+	<script src="../js/popover.js"></script>
+	<script src="../js/collapse.js"></script>
+	<script src="../js/modal.js"></script>
+	<script src="../js/scrollspy.js"></script>
+	<script src="../js/alert.js"></script>
+	<script src="../js/transition.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../javascripts/filter.js" type="text/javascript" charset="utf-8"></script>
+	<script src="../jscript/jquery-1.9.1.js" type="text/javascript"></script>
+
+		<!--Le Facebox-->
+		<link href="../facefiles/facebox.css" media="screen" rel="stylesheet" type="text/css" />
+		<script src="../facefiles/jquery-1.9.js" type="text/javascript"></script>
+		<script src="../facefiles/jquery-1.2.2.pack.js" type="text/javascript"></script>
+		<script src="../facefiles/facebox.js" type="text/javascript"></script>
+		<script type="text/javascript">
+		jQuery(document).ready(function($) {
+		$('a[rel*=facebox]').facebox()
+		})
+		</script>
+</head>
+<body>
+	<div id="header" style="position:fixed;">
+		<img src="../img/logo.jpg">
+		<label>Dream Catchers Zone</label>
+
+			<?php
+				$id = (int) $_SESSION['id'];
+
+					$query = $conn->query ("SELECT * FROM admin WHERE adminid = '$id' ") or die (mysqli_error());
+					$fetch = $query->fetch_array ();
+			?>
+
+			<ul>
+				<li><a href="../function/admin_logout.php"><i class="icon-off icon-white"></i>logout</a></li>
+				<li>Welcome:&nbsp;&nbsp;&nbsp;<i class="icon-user icon-white"></i><?php echo $fetch['username']; ?></a></li>
+			</ul>
+	</div>
+
+	<br>
+
+<div style="position: absolute; top:10.4%" id="leftnav">
+		<ul>
+			<li><a href="admin_home.php" style="color:#333;">Dashboard</a></li>
+			<li><a href="admin_home.php">Products</a>
+				<ul>
+					<li><a href="admin_feature.php "style="font-size:15px; margin-left:15px;">Features</a></li>
+
+					<li><a href="admin_DC.php" style="font-size:15px; margin-left:15px;">DreamCatchers</a></li>
+					<li><a href="admin_DCK.php"style="font-size:15px; margin-left:15px;">(DC)KeyChains</a></li>
+				</ul>
+			</li>
+			<li><a href="transaction.php">Transactions</a></li>
+			<li><a href="customer.php">Customers</a></li>
+			<li><a href="message.php">Messages</a></li>
+			<li><a href="order.php">Orders</a></li>
+		</ul>
+	</div>
+
+ 
