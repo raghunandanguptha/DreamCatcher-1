3bb51eff34f7001016cfc3e5360015f0c9c47d59
Guptha-01 <rguptha2000@gmail.com>
2021-06-23 20:05:26 +0530

    Revert "befor hosting"
    This reverts commit b0962e0541118262322fa8fe1724d33c7d7e42d3.

--- db/dbconn.php
+++ db/dbconn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('sql310.epizy.com', 'epiz_28947689', 'snd1yhpgwN6TFyx', 'epiz_28947689_alphaware');
+	$conn = new mysqli('localhost', 'root', '', 'alphaware');
 	if(!$conn){
 		die("Fatal Error: Connection Error!");
 	}
